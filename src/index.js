750c47c5c911987e87fcd959a82e865113ba8fb5
BuildTools <unconfigured@null.spigotmc.org>
2023-12-22 16:20:04 -0600

    22 12 23 (4:20)

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ app.whenReady().then(() => {
         window = new BrowserWindow({
             width: width * 0.7,
             height: height / 0.7,
-            icon: path.join(__dirname, 'img/sharkcorp.ico')
+            icon: path.join(__dirname, 'img/sharkcorp.ico'),
+            webPreferences: {
+                preload: path.join(__dirname, "preload.js")
+            }
         })
+        window.loadFile(path.join(__dirname,'html/main.html'))
     }
     const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
     createWindow();
+    
 });
